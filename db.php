75a7ee29ec4d92ce461e793db4968e95b3b3f588
hackemistgit <65950565+hackemistgit@users.noreply.github.com>
2025-08-28 22:37:48 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "bb-db";
-$username = "bb-labs-user";
-$password = "bb-labs-password";
-$dbname = "bb-labs";
+$servername = "dpg-d2o8of6mcj7s73baa990-a";
+$username = "lab_e1x0_user";
+$password = "Em7tocSuPyFi127HChLNV1HiU30pytNO";
+$dbname = "lab_e1x0";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
